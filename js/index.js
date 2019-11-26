d2700ed3fae698a1baab43cc114f7872f16aa1eb
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-26 21:51:48 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,3 +2,6 @@ let divs = document.querySelectorAll('div');
 function bubble(e){
 console.log(this.firstChild.nodeValue.trim() + 'bubbled');
 }
+for (let i = 0; i < divs.length; i++){
+  divs[i].addEventListener('click', bubble);
+}
