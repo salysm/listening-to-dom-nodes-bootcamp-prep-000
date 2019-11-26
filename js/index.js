fc391144b335df7c7a0ac2f93f579807325d19d8
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-26 21:49:59 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-const main = document.getElementById('main');
-main.addEventListener('click', function(event){
-  alert('I was clicked!');
-});
+let divs = document.querySelectorAll('div');
+function bubble(e)
+
